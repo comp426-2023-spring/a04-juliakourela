18c212917a497f55d62920cf2a6729de90af7273
Jules Kourelakos <julia.kourelakos@duke.edu>
2023-03-23 10:13:32 -0400

    do i need json stringify?

--- server.js
+++ server.js
@@ -10,47 +10,47 @@ const port = args.port || 5000;
 
 // default check
 app.get('/app', (req, res) => {
-    res.status(200).send(rps(null));
+    res.status(200).send('200 OK');
 })
 
 //rps with no args
 app.get('/app/rps', (req, res) => {
-    res.status(200).send(rps());
+    res.status(200).send(JSON.stringify(rps()));
 })
 
 //rps with arg
 app.get('/app/rp/play', (req, res) => {
-    res.status(200).send(rps(req.query.shot));
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));
 })
 
 //rpsls with no args
 app.get('/app/rpsls/play', (req, res) => {
-    res.status(200).send(rpsls());
+    res.status(200).send(JSON.stringify(rpsls()));
 })
 
 //rpsls with arg
 app.get('/app/rpsls/play', (req, res) => {
-    res.status(200).send(rpsls(req.query.shot));
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
 })
 
 //post rps
 app.post('/app/rps/play', (req, res) => {
-    res.status(200).send(rpsls(req.query.shot));
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
 })
 
 //post rpsls
 app.post('/app/rpsls/play', (req, res) => {
-    res.status(200).send(rpsls(req.body.shot));
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
 })
 
 // get params for rps
 app.get('/app/rps/play/:shot', (req, res) => {
-    res.status(200).send(rps(req.params.shot));
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));
 })
 
 // get params for rpsls
 app.get('/app/rpsls/play/:shot', (req, res) => {
-    res.status(200).send(rpsls(req.params.shot));
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
 })
 
 // undefined endpoint
