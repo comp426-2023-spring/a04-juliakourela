500fdb6026491b95881308c6073ced7429105f11
Jules Kourelakos <julia.kourelakos@duke.edu>
2023-03-23 10:05:14 -0400

    test of get/post infrastructure

--- server.js
+++ server.js
@@ -8,14 +8,54 @@ app.use(express.json());
 let args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
 
-// undefined endpoint
-app.get('*', (req, res) => {
-    res.status(404).send("404 NOT FOUND");
-})
-
 // default check
 app.get('/app', (req, res) => {
     res.status(200).send(rps(null));
+})
+
+//rps with no args
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(rps());
+})
+
+//rps with arg
+app.get('/app/rp/play', (req, res) => {
+    res.status(200).send(rps(req.query.shot));
+})
+
+//rpsls with no args
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls());
+})
+
+//rpsls with arg
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.query.shot));
+})
+
+//post rps
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(rpsls(req.query.shot));
+})
+
+//post rpsls
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.body.shot));
+})
+
+// get params for rps
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(rps(req.params.shot));
+})
+
+// get params for rpsls
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(rpsls(req.params.shot));
+})
+
+// undefined endpoint
+app.get('*', (req, res) => {
+    res.status(404).send("404 NOT FOUND");
 })
 
 app.listen(port, () => {
