a23403d324385010e6053876f3f266de439795ff
Jules Kourelakos <julia.kourelakos@duke.edu>
2023-03-22 23:37:39 -0400

    undefiend api endpoint

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.use(express.json());
 let args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
 
+// undefined endpoint
+app.get('*', (req, res) => {
+    res.status(404).send("404 NOT FOUND");
+})
+
+// default check
 app.get('/app', (req, res) => {
     res.status(200).send(rps(null));
 })
